6799ddaba014b9c8d0e3bf6c8a4729c91f64f223
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-05 11:33:18 -0500

    added a name attribute  to the input tag

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -5,9 +5,9 @@ state = {
     title: ""
 };
 
-onChange = (e) => { //event handler for onChange prop in the input tag
+onChange = (e) => { //event handler for onChange prop in the input tag, triggered by any value placed in the onChange input field.
     this.setState({
-        title: e.target.value
+        [e.target.name]: e.target.value
     }); //setState
 }; //onChange method (part of class)
 
@@ -18,6 +18,7 @@ onChange = (e) => { //event handler for onChange prop in the input tag
                     type="text" 
                     placeholder="Add Todo..." 
                     value={this.state.title}
+                    name="title" //why isn't this {this.state.title}?
                     onChange={this.onChange}
                 /> 
                 <input type="submit" value="Submit" />
