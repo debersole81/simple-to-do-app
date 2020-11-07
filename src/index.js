96c3bd77a793555052c7fd69afc5774488a3f851
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-07 05:27:10 -0500

    imported App.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react" //importing React from the react module
 import ReactDOM from "react-dom" //importing ReactDOM from the react-dom module
+//component file
 import TodoContainer from "./components/TodoContainer"
+import "./App.css"
 
 
 ReactDOM.render(<TodoContainer />, document.getElementById("root"))
