a24526680d8a7b9040adc20481f3209ce809a317
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-09 19:32:48 -0500

    replaced this.props.todo with their corresponding variables

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -10,20 +10,18 @@ class TodoItem extends React.Component {
         } //completedStyle object
 
         const { completed, id, title} = this.props.todo //destructuring the this.props.todo (i.e., pulling variables out of the todo)
-        
+
         return( 
         <li className="todo-item">
             <input 
                 type="checkbox"
-                checked={this.props.todo.completed}
-                onChange={() => this.props.handleChange(this.props.todo.id)} //onChange handler calls the handleChange method in TodoContainer and passes todo id.
+                checked={completed}
+                onChange={() => this.props.handleChange(id)} //onChange handler calls the handleChange method in TodoContainer and passes todo id.
             /> 
-            <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}> 
+            <button onClick={() => this.props.deleteTodoProps(id)}> 
                 Delete
             </button> 
-            <span style={this.props.todo.completed ? completedStyle : null}>
-                {this.props.todo.title}
-            </span>
+            <span style={completed ? completedStyle : null}>{title}</span>
         </li>
         ) //return method
     } //render method
