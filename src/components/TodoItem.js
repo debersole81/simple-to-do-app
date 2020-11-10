a52dca556cfd97401d6824225e19c0daa0a80539
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-09 19:13:23 -0500

    created an object to handle line styling for completed objects

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,14 @@ import React from "react"
 
 class TodoItem extends React.Component {
     render() {
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#d35e0f",
+            opacity: 0.4,
+            textDecoration: "line-through",
+
+        }
+
         return <li className="todo-item">
             <input 
                 type="checkbox"
