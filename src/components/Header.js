6a47a069d299ac8e44bc57ac9fa1bc27004facec
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-09 19:07:45 -0500

    created an object to handle styling for the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from "react"
 
 let Header = () => {
+    
+    const headerStyle = {
+        padding: "20px 0",
+        lineHeight: "2em",
+    }
+
     return (
-        <header>
+        <header style={headerStyle}>
             <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>Simple Todo App</h1>
             <p style={{ fontSize: "19px" }}>Please add to-dos item(s) through the input field</p>
         </header>
