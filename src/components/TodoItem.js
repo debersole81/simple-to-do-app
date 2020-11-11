690f34f1ef685bb683aa93b928112940952f09d8
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-10 19:43:54 -0500

    updated comments

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,24 +7,24 @@ class TodoItem extends React.Component {
             color: "#d35e0f",
             opacity: 0.4,
             textDecoration: "line-through",
-        } //completedStyle object
+        }
 
-        const { completed, id, title} = this.props.todo //destructuring the this.props.todo (i.e., pulling variables out of the todo)
+        const {completed, id, title} = this.props.todo
 
         return( 
         <li className="todo-item">
             <input 
                 type="checkbox"
                 checked={completed}
-                onChange={() => this.props.handleChange(id)} //onChange handler calls the handleChange method in TodoContainer and passes todo id.
+                onChange={() => this.props.handleChange(id)}
             /> 
             <button onClick={() => this.props.deleteTodoProps(id)}> 
                 Delete
             </button> 
             <span style={completed ? completedStyle : null}>{title}</span>
         </li>
-        ) //return method
-    } //render method
-} //TodoItem
+        )
+    }
+}
 
 export default TodoItem
