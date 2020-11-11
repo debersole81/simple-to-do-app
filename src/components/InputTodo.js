b808cf9cd33bab24d6a32754123d5c73efec354f
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-10 20:02:10 -0500

    updated comments

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -5,19 +5,19 @@ state = {
     title: ""
 };
 
-onChange = (e) => { //event handler for onChange prop in the input tag, triggered by any value placed in the onChange input field.
+onChange = (e) => {
     this.setState({
         [e.target.name]: e.target.value
-    }); //setState
-}; //onChange method (part of class)
+    });
+};
 
 handleSubmit = (e) => {
     e.preventDefault();
     this.props.addTodoItem(this.state.title);
     this.setState({
         title:""
-    }) //setState method
-};//handleSubmit method
+    })
+};
 
     render() {
         return (
@@ -27,7 +27,7 @@ handleSubmit = (e) => {
                     className="input-text" 
                     placeholder="Add Todo..." 
                     value={this.state.title}
-                    name="title" //why isn't this {this.state.title}?
+                    name="title"
                     onChange={this.onChange}
                 /> 
                 <input type="submit" className="input-submit" value="Submit" />
