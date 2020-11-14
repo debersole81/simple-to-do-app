31cb071f017d0de8ff66bec2eb8aee1450e446e1
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-11-14 08:00:39 -0500

    changed Header component from functional to class

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
-import React from "react"
+import React, { Component } from "react"
 
-let Header = () => {
-    
-    const headerStyle = {
+class Header extends Component {
+    render() {
+      const headerStyle = {
         padding: "20px 0",
         lineHeight: "2em",
-    }
-
-    return (
+      }
+      return (
         <header style={headerStyle}>
-            <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>Simple Todo App</h1>
-            <p style={{ fontSize: "19px" }}>Please add to-dos item(s) through the input field</p>
+          <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>
+            Simple Todo App <span id="inH1"></span>
+          </h1>
+          <p style={{ fontSize: "19px" }}>
+            Please add to-dos item(s) through the input field
+          </p>
         </header>
-    )
-}
+      )
+    }
+  }
+         
+
 
 export default Header
